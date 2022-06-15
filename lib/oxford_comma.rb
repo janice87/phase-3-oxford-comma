4a502c7588f13b5ebe041955509633b13c15f3ab
Janice Chan <janice.chan1288@gmail.com>
2022-06-15 10:00:37 -0400

    commit tree 89edd52f79f695263a63ee3e0418b38f86dbf63a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,35 @@
-def oxford_comma(array)
+# split method: string => array
+# join method:  array => string
 
+# want array to convert to string
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"]) 
+# # => "fiddleheads, okra, and kohlrabi" 
+
+# oxford_comma(["fiddleheads", "okra"]) 
+# => "fiddleheads and okra" 
+ 
+def oxford_comma(array)
+    if array.length < 3
+      array.join(" and ")
+    else 
+        array[-1] = "and #{array.last}" # or "and #{array[-1]}" 
+        array.join(", ")
+    end
 end
+
+# also works
+# def oxford_comma(array)
+#     return array.join(" and ") if array.length < 3
+#     array[-1] = "and #{array[-1]}"
+#     array.join(", ")
+# end
+
+
+# testing out code for length > 3
+# def oxford_comma(array)
+#     array[-1] = "and #{array[-1]}"
+#     array.join(", ")
+# end
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+
+
